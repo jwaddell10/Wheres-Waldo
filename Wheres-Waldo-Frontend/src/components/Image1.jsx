8b61b6dc35062f13ea3f6a4a801766cf27968582
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-06-27 12:08:30 -0500

    forgot to commit this

--- Wheres-Waldo-Frontend/src/components/Image1.jsx
+++ Wheres-Waldo-Frontend/src/components/Image1.jsx
@@ -1,18 +1,63 @@
 import { Link } from "react-router-dom";
-import waldoBeach from "../assets/waldoBeach.jpg"
+import { useState } from "react";
+import waldoBeach from "../assets/waldoBeach.jpg";
+import styled from "styled-components";
 
 function Image1() {
+	const [xCoordinates, setXCoordinates] = useState(null);
+	const [yCoordinates, setYCoordinates] = useState(null);
+	const [dropDownVisible, setDropDownVisible] = useState(false);
 
+	const handleClick = (e) => {
+		const xCoordinate = e.clientX;
+		const yCoordinate = e.clientY;
+
+		setXCoordinates(xCoordinate);
+		setYCoordinates(yCoordinate);
+		setDropDownVisible(!dropDownVisible);
+		//need to have dropdown
+		//need to have target box
+	};
 	return (
 		<>
 			<header>
 				<Link to="/">Return Home</Link>
 			</header>
 			<div className="image1">
-				<img src={waldoBeach} alt="" />
-			</div>	
+				<img
+					src={waldoBeach}
+					onClick={(e) => {
+						handleClick(e);
+					}}
+					alt=""
+				/>
+				{dropDownVisible && (
+					<DropDown
+						xCoordinates={xCoordinates}
+						yCoordinates={yCoordinates}
+					/>
+				)}
+			</div>
 		</>
 	);
 }
+
+function DropDown({ xCoordinates, yCoordinates }) {
+	return (
+		<DropDownStyled x={xCoordinates} y={yCoordinates}>
+			<div className="dropDown">
+				<option value="Waldo">Waldo</option>
+				<option value="Wizard">Wizard</option>
+				<option value="Odlaw">Odlaw</option>
+			</div>
+		</DropDownStyled>
+	);
+}
+
+const DropDownStyled = styled.section`
+	top: ${(props) => props.y}px;
+	left: ${(props) => props.x}px;
+	position: absolute;
+`;
 
 export default Image1;
