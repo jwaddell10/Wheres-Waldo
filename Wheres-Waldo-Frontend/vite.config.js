8f6f6fd9ab036e6cc7115f9afac3e400c2dad722
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-09-04 16:28:13 -0500

    update build

--- Wheres-Waldo-Frontend/vite.config.js
+++ Wheres-Waldo-Frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
-  assetsInclude: ['**/*.jpg'],
   plugins: [react()],
 })
